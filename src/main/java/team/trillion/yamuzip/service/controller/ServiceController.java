20fca50f77c09ac76e6b8be4b8ee64ce6ab7236f
EUNJAE97 <154950170+EUNJAE97@users.noreply.github.com>
2024-03-27 17:50:51 +0900

    Feat: servicePage 리스트 조회/상세페이지
    Realated to : #4

--- src/main/java/team/trillion/yamuzip/service/controller/ServiceController.java
+++ src/main/java/team/trillion/yamuzip/service/controller/ServiceController.java
@@ -33,8 +33,8 @@ public class ServiceController {
         return "search/serviceList";
     }
 
-    @GetMapping("/serviceInfo")
-    public String findInfoService(@RequestParam("code") long serviceCode, Model model) {
+    @GetMapping("/serviceInfo/{serviceCode}")
+    public String findInfoService(@PathVariable("serviceCode") long serviceCode, Model model) {
         List<ServiceDTO> serviceInfoList = serviceService.findInfoService(serviceCode);
 
 
